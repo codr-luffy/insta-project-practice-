ff386ae407b6ebc7c4e4fdcbd32892e2431f6b1d
luffy_codr <dev.luffycodr@gmail.com>
2026-04-06 14:38:05 +0530

    stage-3 Post creation
    post creation with who is creating verification

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -1,6 +1,7 @@
 const postModel = require("../models/post.model.js");
 const imageKit = require("@imagekit/nodejs");
 const { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
 
 const imagekit = new imageKit({
   privateKey: process.env.IMG_KEY,
@@ -9,12 +10,39 @@ const imagekit = new imageKit({
 async function createPostController(req, res) {
   console.log(req.body, req.file);
 
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({
+      message: "Token Not provided,Unauthorised Activity",
+    });
+  }
+
+  let decoded;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: "user not authorised",
+    });
+  }
+
   const file = await imagekit.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
     fileName: "Test",
+    folder: "insta-clone",
   });
 
-  res.send(file);
+  const post = await postModel.create({
+    caption: req.body.caption,
+    imageUrl: file.url,
+    user: decoded.id,
+  });
+
+  res.status(201).json({
+    message: "Post Creatted sucessfully",
+    post,
+  });
 }
 
 module.exports = {
